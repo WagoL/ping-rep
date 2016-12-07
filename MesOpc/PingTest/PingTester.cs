019b75146afe50088d756f61c8e870da94b792ba
WagoL <wago@live.be>
2016-12-07 16:03:49 +0100

    added ping class

---
+++ MesOpc/PingTest/PingTester.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingTest
+{
+    public class PingTester
+    {
+        private string _ipAddress;
+
+        public PingTester(string ip)
+        {
+            _ipAddress = ip;
+        }
+
+        public bool Execute()
+        {
+            bool pingAble = false;
+            Ping pinger = new Ping();
+            try
+            {
+                PingReply reply = pinger.Send(_ipAddress);
+                pingAble = reply.Status == IPStatus.Success;
+            }
+            catch (PingException) { }
+            return pingAble;
+        }
+
+    }
+}
